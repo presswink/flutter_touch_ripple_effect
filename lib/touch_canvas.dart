afefc73c126e2c8c5ffe7d18cd29dfad2b1484f6
raj68518 <raj68518@gmail.com>
2021-01-24 12:19:36 +0530

    touch canvas file created to work with canvas

---
+++ lib/touch_canvas.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TouchCanvas extends StatefulWidget {
+  TouchCanvas({Key key}) : super(key: key);
+
+  @override
+  _TouchCanvasState createState() => _TouchCanvasState();
+}
+
+class _TouchCanvasState extends State<TouchCanvas> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: null,
+    );
+  }
+}
