9afa13e650a72d88374e385a7ab282eb2904769f
raj68518 <raj68518@gmail.com>
2021-01-25 12:55:02 +0530

    touch_feedback doc added

--- lib/src/touch_feedback.dart
+++ lib/src/touch_feedback.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
 class TouchFeedback extends StatefulWidget {
+
+  /// [child] user child widget
+  /// [rippleColor] touch ripple color of widget
+  /// [backgroundColor] of widget container
+  /// if you have border of child widget then you should apply [borderRadius]
+  /// [feedbackDuration] will be animation duration.
+  /// [onTap] is for user click or tap handle.
+
   final Widget child;
   final Color rippleColor;
   final Color backgroundColor;
@@ -18,9 +26,15 @@ class TouchFeedback extends StatefulWidget {
 }
 
 class _TouchFeedbackState extends State<TouchFeedback> {
+  /// private [_globalKey] global variable initialized
   GlobalKey _globalKey = GlobalKey();
+
+  /// private [_rippleWidget] global variable initialized
   Widget _rippleWidget;
+
+  /// user tap private [_dx]  x-axis global variable initalized
   double _dx;
+   /// user tap private [_dy]  y-axis global variable initalized
   double _dy;
 
   double _mWidth;
@@ -29,13 +43,8 @@ class _TouchFeedbackState extends State<TouchFeedback> {
   double _animWidth;
   double _animHeight;
 
+  /// private [_defaultDuration] duration of animation if user not assign
   Duration _defaultDuration = Duration(milliseconds: 200);
-
-  @override
-  void initState() { 
-    super.initState();
-
-  }
 
   void _generateRipple(){
     
@@ -47,12 +56,13 @@ class _TouchFeedbackState extends State<TouchFeedback> {
           height: _animWidth == _mHeight ? 10: _animHeight,
           alignment: Alignment.center,
           decoration: BoxDecoration(color: widget.rippleColor, borderRadius: BorderRadius.circular(5)),
-          duration: widget.duration != null ? widget.duration: _defaultDuration,
+          duration: widget.feedbackDuration != null ? widget.feedbackDuration: _defaultDuration,
           curve: Curves.easeInQuint,
           ),
       );
     });
     milisecons();
+    // resetting axis after animation
     _dx= 0;
     _dy =0;
   }
@@ -71,16 +81,21 @@ class _TouchFeedbackState extends State<TouchFeedback> {
 
   void _setChildSize(){
     setState(() {
+      /// sets child widget width to [_mWidth]
       _mWidth = _globalKey.currentContext.size.width;
+       /// sets child widget Height to [_mHeight]
       _mHeight = _globalKey.currentContext.size.height;
+      /// setting [_mWidth] and [_mHeight] to [_animWidth] and [_animHeight]
       _animWidth = _mWidth;
       _animHeight = _mHeight;
     });
   }
 
+  /// hide rippleWidget when time-up
+
   void _timeOut(){
     Future.delayed(
-      widget.duration != null ? widget.duration : _defaultDuration,
+      widget.feedbackDuration != null ? widget.feedbackDuration : _defaultDuration,
       (){
         setState(() {
           _rippleWidget = null;
