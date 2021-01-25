7063bc4ace65b1acaf62baa4fdefa8fb6a25788f
raj68518 <raj68518@gmail.com>
2021-01-25 16:08:40 +0530

    example clreated for both

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,12 +25,35 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: TouchFeedback(
-
-          )
+        body: Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              /// touch ripple effect implimented
+              TouchRippleEffect(
+                rippleColor: Colors.white60,
+                child: IconButton(iconSize: 24.0, icon: Icon(Icons.search,),color: Colors.pink, onPressed: null),
+              ),
+              TouchFeedback(
+                rippleColor: Colors.blue[200],
+                child: Container(
+                  width: 80,
+                  height: 40, 
+                  alignment: Alignment.centerLeft, 
+                  color: Colors.yellow, 
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(5),), 
+                  child: Text(
+                    "Hit me !", 
+                    style: TextStyle(fontSize: 20, fontWeight:  FontWeight.bold)
+                    )
+                    ),
+              )
+            ],
+          ),
+        )
         ),
-      ),
-    );
+      );
   }
 }
