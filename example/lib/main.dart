e0893de7464e7ece0993126ff8b2cd6106b68fbc
adityapanther <raj68518@gmail.com>
2023-07-02 22:26:04 +0530

    No Directionality widget found

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-MyApp({Key key}) : super(key: key);
+  MyApp({Key key}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
@@ -19,54 +19,58 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Container(
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              /// touch ripple effect implimented
-              
-              TouchRippleEffect(
+          body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          textDirection: TextDirection.ltr,
+          children: [
+            /// touch ripple effect implimented
+
+            TouchRippleEffect(
                 borderRadius: _helloRadius,
                 rippleColor: Colors.white60,
-                onTap: (){
+                onTap: () {
                   print("adi !");
                 },
                 child: Container(
-                  width: 110, 
-                  height: 50, 
-                  alignment: Alignment.center, 
-                  decoration: BoxDecoration(color: Colors.pink, borderRadius: _helloRadius),
-                  child: IconButton(
-                    iconSize: 24.0, 
-                    icon: Icon(Icons.search,color: Colors.white, size: 36,), 
-                    onPressed: null
-                    ),)
-              ),
-
-              /// touch Feedback effect implimented.
-              TouchFeedback(
-                onTap: (){
-                  print(" I am Aditya");
-                },
-                rippleColor: Colors.blue[200],
-                child: Container(
-                  width: 120,
-                  height: 40, 
+                  width: 110,
+                  height: 50,
                   alignment: Alignment.center,
-                  decoration: BoxDecoration(color: Colors.yellow, borderRadius: BorderRadius.circular(5),), 
-                  child: Text(
-                    "Hit me !", 
-                    style: TextStyle(fontSize: 20, fontWeight:  FontWeight.bold)
-                    )
-                    ),
-              )
-            ],
-          ),
-        )
+                  decoration: BoxDecoration(
+                      color: Colors.pink, borderRadius: _helloRadius),
+                  child: IconButton(
+                      iconSize: 24.0,
+                      icon: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                        size: 36,
+                      ),
+                      onPressed: null),
+                )),
+
+            /// touch Feedback effect implimented.
+            TouchFeedback(
+              onTap: () {
+                print(" I am Aditya");
+              },
+              rippleColor: Colors.blue[200],
+              child: Container(
+                  width: 120,
+                  height: 40,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Text("Hit me !",
+                      style: TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold))),
+            )
+          ],
         ),
-      );
+      )),
+    );
   }
 }
-
