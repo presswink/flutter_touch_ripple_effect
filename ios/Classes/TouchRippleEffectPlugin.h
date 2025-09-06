2950b781d606e5bbc69b67aadeb9678d929526c0
adityakumar <raj68518@gmail.com>
2025-09-06 16:47:08 +0530

    migrate from plugins to package

--- ios/Classes/TouchRippleEffectPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface TouchRippleEffectPlugin : NSObject<FlutterPlugin>
-@end
---
+++ test/flutter_touch_ripple_effect_test.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:touch_ripple_effect/flutter_touch_ripple_effect.dart';
+
+void main() {
+  testWidgets('testing TouchRippleEffect widget', (tester) async {
+    await tester.pumpWidget(MaterialApp(home: TouchRippleEffect(
+      rippleColor: Colors.yellow,
+      child: SizedBox(width: 400, height: 400, child: Icon(Icons.add), ),
+    ),));
+    expect(find.byIcon(Icons.add), findsOneWidget);
+  });
+
+  testWidgets('testing TouchFeedback widget', (tester) async {
+    await tester.pumpWidget(MaterialApp(home: TouchFeedback(child: SizedBox(width: 400, height: 400, child: Icon(Icons.remove),),),));
+    expect(find.byIcon(Icons.remove), findsOneWidget);
+  });
+}
+
+
